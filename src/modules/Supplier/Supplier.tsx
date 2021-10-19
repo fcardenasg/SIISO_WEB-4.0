6615c4cb5ecdb8e0ae9dd0cb60d50c4ad7338242
fcardenasg <fcardenas_g@hotmail.com>
2021-10-19 18:13:22 -0500

    chip

--- src/modules/Supplier/Supplier.tsx
+++ src/modules/Supplier/Supplier.tsx
@@ -13,7 +13,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
+
 
 // Validacion
 import * as Yup from "yup";
@@ -35,31 +35,6 @@ const city: SelectOptions[] = [
   },
 ];
 
-// chip implementacion
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-// Cargar combo chip
-const names = [
-  "Audiometria",
-  "Visiometria",
-  "Espirometria",
-  "RX Torax",
-  "Laboratorios Clinicos",
-  "Electrocardiogramas",
-  "RX CLS",
-  "RNM",
-  "Resonancia Magnetica",
-  "Medicamentos",
-];
 
 type SupplierForm = {
   NombProv: string;
@@ -117,41 +92,21 @@ const Supplier = () => {
     console.log(data);
   };
 
-  function getStyles(name: string, personName: readonly string[], theme: Theme) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    };
-  }
+  
 
-  function MultipleSelectChip() {
-    const theme = useTheme();
-    const [personName, setPersonName] = React.useState<string[]>([]);
-
-    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-      const {
-        target: { value },
-      } = event;
-      setPersonName(
-        // On autofill we get a the stringified value.
-        typeof value === "string" ? value.split(",") : value
-      );
-    };
-  }
+ 
   //mostrar en pantalla
   return (
-    <div className="p-10 flex justify-center items-center">
-      <div className="md:w-1/2 bg-white rounded shadow py-5 px-10">
+    <div className="p-1 flex justify-center items-center">
+      <div className="md:w-full xl bg-white rounded shadow py-5 px-10">
         <div className="w-full flex justify-center">
-          <span className="text-gray-700 font-montserrat font-semibold text-lg text-center">
+          <span className="text-gray-600 font-montserrat font-semibold text-lg text-center">
             Registro de Proveedores
           </span>
         </div>
         {/* Linea roja y texto */}
         <div className="grid grid-cols-2 gap-2">
-          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-2">
+          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-5">
             <InputText
               control={control}
               name="Código"
@@ -167,7 +122,7 @@ const Supplier = () => {
               defaultValue=""
             />
           </div>
-          <div className="border rounded border-red-200 flex flex-col px-4 pt-2">
+          <div className="border rounded border-red-200 flex flex-col px-4 pt-5">
             <InputText
               control={control}
               name="Celular"
@@ -183,7 +138,7 @@ const Supplier = () => {
               defaultValue=""
             />
           </div>
-          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-2">
+          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-5">
             <InputText
               control={control}
               name="Contacto"
@@ -200,7 +155,7 @@ const Supplier = () => {
               errorMessage={errors.CiudProv?.message}
             />
           </div>
-          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-2">
+          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-5">
             <InputText
               control={control}
               name="Dirección"
@@ -208,38 +163,14 @@ const Supplier = () => {
               label="Dirección"
               defaultValue=""
             />
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
-              <Select
-                labelId="demo-multiple-chip-label"
-                id="demo-multiple-chip"
-                multiple
-                value={personName}
-                onChange={handleChange}
-                input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                renderValue={(selected) => (
-                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                    {selected.map((value) => (
-                      <Chip key={value} label={value} />
-                    ))}
-                  </Box>
-                )}
-                MenuProps={MenuProps}
-              >
-                {names.map((name) => (
-                  <MenuItem
-                    key={name}
-                    value={name}
-                    style={getStyles(name, personName, theme)}
-                  >
-                    {name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+           
+            {/* Aqui va el chip */}
+            
+           
+          
           </div>
       
-          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-2">
+          <div className="border rounded border-red-200 flex flex-col flex-1 px-4 pt-5">
             <InputCheck
               control={control}
               name="Estado"
