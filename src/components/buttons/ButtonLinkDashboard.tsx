d49a677a39583f080a91586c28a09f2e11d9aede
fcardenasg <fcardenas_g@hotmail.com>
2021-10-21 14:41:06 -0500

    colores boton

--- src/components/buttons/ButtonLinkDashboard.tsx
+++ src/components/buttons/ButtonLinkDashboard.tsx
@@ -18,7 +18,7 @@ export const ButtonLinkDashboard: React.FC<Props> = ({
   return (
     <Tooltip title={subtitle}>
       <button onClick={onPress}>
-        <div className="bg-red-1 h-24 flex flex-row flex-1 rounded-xl shadow text-white font-montserrat  p-2 hover:bg-white hover:text-gray-700 hover:shadow-lg cursor-pointer">
+        <div className="bg-gray-500 h-24 flex flex-row flex-1 rounded-xl shadow text-white font-montserrat  p-2 hover:bg-red-1 hover:text-white hover:shadow-lg cursor-pointer">
           <div className="flex flex-col flex-1 justify-end flex-wrap">
             <span className="text-sm font-montserrat">{title}</span>
           </div>
