d580c78d83059fa4fc653d49972020968305a735
carlos <carlosjpitre@gmail.com>
2021-10-23 13:05:25 -0500

    Listado

---
+++ src/types/ProgrammingType.ts
@@ -0,0 +1,14 @@
+export type Programming = {
+  ImagenUrl: string;
+	Documento: string;
+	Nombres: string;
+	EstadoCaso: string;
+	Contingencia: string;
+	EstadoPac: string;
+	Atencion: string[];
+	Celular: string;
+	Fecha: string;
+	Sede: string;
+	usuario: string;
+}
+
