c716aeb0954a961c98f2ab879e5c5a74f545d30b
ManuelVquez <manuelvasquez107@gmail.com>
2021-11-23 18:31:16 -0500

    Ultimos cambios

--- src/modules/Companies/Companies.tsx
+++ src/modules/Companies/Companies.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
 import { InputText } from "../../components/input/InputText";
 import { ButtonPrimary } from "../../components/buttons/ButtonPrimary";
 import { Link } from "react-router-dom";
@@ -54,10 +55,12 @@ const Companies = () => {
     console.log(data);
   };
 
+  const history = useHistory();
+
   //mostrar en pantalla
   return (
     <div className="p-10 flex justify-center items-center">
-      <div className="md:w-1/2 bg-white rounded shadow py-5 px-10">
+      <div className="md:w-full xl bg-white rounded shadow py-5 px-10">
         <div className="w-full flex justify-center">
           <span className="text-gray-700 font-montserrat font-semibold text-lg text-center">
             Registro de Empresas
@@ -65,57 +68,61 @@ const Companies = () => {
         </div>
         {/* Linea roja y texto */}
         <div className="w-full border-b-2 border-red-1 my-4"></div>
-        <InputText
-          control={control}
-          name="Codigo"
-          errorMessage={errors?.Codigo?.message}
-          label="Código"
-          defaultValue=""
-        />
-        <InputText
-          control={control}
-          name="Nombre"
-          errorMessage={errors?.DescripcionSpa?.message}
-          label="Nombre"
-          defaultValue=""
-        />
-        <InputText
-          control={control}
-          name="Correo electrónico"
-          errorMessage={errors?.Email?.message}
-          label="Correo electrónico"
-          defaultValue=""
-        />
-        <InputText
-          control={control}
-          name="Celular"
-          errorMessage={errors?.Celular?.message}
-          label="Celular"
-          defaultValue=""
-        />
-        <InputText
-          control={control}
-          name="Contacto"
-          errorMessage={errors?.Gerente?.message}
-          label="Contacto"
-          defaultValue=""
-        />
+        <div className="grid grid-cols-3 gap-4 my-6">
+          <InputText
+            control={control}
+            name="Codigo"
+            errorMessage={errors?.Codigo?.message}
+            label="Código"
+            defaultValue=""
+          />
+          <InputText
+            control={control}
+            name="Nombre"
+            errorMessage={errors?.DescripcionSpa?.message}
+            label="Nombre"
+            defaultValue=""
+          />
+          <InputText
+            control={control}
+            name="Correo electrónico"
+            errorMessage={errors?.Email?.message}
+            label="Correo electrónico"
+            defaultValue=""
+          />
+          <InputText
+            control={control}
+            name="Celular"
+            errorMessage={errors?.Celular?.message}
+            label="Celular"
+            defaultValue=""
+          />
+          <InputText
+            control={control}
+            name="Contacto"
+            errorMessage={errors?.Gerente?.message}
+            label="Contacto"
+            defaultValue=""
+          />
 
-        <InputCheck
-          control={control}
-          name="Estado"
-          label="Estado"
-          defaultValue={false}
-          errorMessage={errors.Estado?.message}
-        />
+          <InputCheck
+            control={control}
+            name="Estado"
+            label="Estado"
+            defaultValue={false}
+            errorMessage={errors.Estado?.message}
+          />
+        </div>
 
-        <ButtonPrimary onPress={handleSubmit(handleClick)} text="Guardar" />
-        <div className="h-3"></div>
-        <Link to="/Parameterization">
-          <ButtonOutline onPress={() => {}} text="Cancelar" />
-        </Link>
+        <div className="flex flex-row items-center justify-center">
+        <ButtonOutline
+            onPress={() => history.push("/Companies")}
+            text="Cerrar" />
+          <div className="h-3"></div>
+          <ButtonPrimary onPress={handleSubmit(handleClick)} text="Guardar" />
+        </div>
       </div>
-    </div>
+    </div >
   );
 };
 
